030d61a9d5915ee43d98ff4c0dafb8fb3f5753ad
Danilo Oliveira <oliveira.83555@gmail.com>
2022-02-16 22:32:56 -0300

    arrumando o carrinho

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,28 +1,29 @@
 import { Badge } from "@material-ui/core";
 import { SearchRounded, ShoppingCart } from "@material-ui/icons";
-import { Center, Container, Left, Right, Wrapper, MenuItem } from "./style";
+import { Center, Container, Left, Right, Wrapper, MenuItem, Cart } from "./style";
 
 export function Navbar() {
   return (
     <Container>
       <Wrapper>
           <Left>
-              <input type="text" placeholder="Search Products"/>
+              <input type="text" placeholder="Search"/>
               <SearchRounded style={{color:"#fff", fontSize:"30px ", cursor:"pointer"}} />
           </Left>
           <Center>
             <h1>H.M</h1>
           </Center>
           <Right>
-            <MenuItem>REGISTRE-SE</MenuItem>
+            <MenuItem>Registrar</MenuItem>
             <MenuItem>Entrar</MenuItem>
-            <MenuItem>
-              <Badge badgeContent={0} 
+            <Cart>
+              <Badge 
+              badgeContent={3} 
               color="primary"
               >
                 <ShoppingCart />
               </Badge>
-            </MenuItem>
+            </Cart>
           </Right>
       </Wrapper>
     </Container>
