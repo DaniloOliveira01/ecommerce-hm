4ea3cd155c2e749cb837295775a32a5ab894882e
Danilo Oliveira <oliveira.83555@gmail.com>
2022-02-16 22:36:56 -0300

    adicionando a responsividade

--- src/Pages/Cart/style.ts
+++ src/Pages/Cart/style.ts
@@ -1,11 +1,13 @@
 import styled from "styled-components";
+import { mobile } from "../../styles/Responsive";
 
 export const Container = styled.div`
-  color: #fff;
+  color: #000;
 `;
 
 export const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({ width: "75%"})}
 `;
 
 export const Title = styled.h1`
@@ -21,23 +23,20 @@ export const Top = styled.div`
 `;
 
 export const TopButton = styled.button`
-  padding: 15px;
-  font-size: 20px;
+  padding: 10px;
   font-weight: 600;
   cursor: pointer;
-  border: none;
-  border-radius: 8px;
-  background-color: #e2e2e2;
-  color: #000;
-  transition: all 0.4s ease;
+  border: ${(props) => props.typeof === "filled" && "none"};
+  background-color: ${(props) => props.typeof === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.typeof === "filled" && "white"};
 
-  &:hover {
-    background-color: rgba(0,0,0,0.5);
-    color: #e2e2e2;
-  }
+
+${mobile({ margin: "0 12px"})}
 `;
 
-export const TopTexts = styled.div``;
+export const TopTexts = styled.div`
+  ${mobile({ display: "none"})}
+`;
 
 export const TopText = styled.span`
   font-size: 20px;
@@ -53,7 +52,7 @@ export const TopText = styled.span`
 export const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
-
+  ${mobile({ flexDirection: "column"})}
 `;
 
 export const Info = styled.div`
@@ -63,7 +62,8 @@ export const Info = styled.div`
 export const Product = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 20px;
+  padding: 10px 0;
+  ${mobile({ flexDirection: "column"})}
 `;
 
 export const ProductsDetails = styled.div`
@@ -114,11 +114,13 @@ export const ProductAmount = styled.div`
 export const ProductAmountQuant = styled.span`
   font-size: 24px;
   margin: 5px;
+  ${mobile({ margin: "5px 15px"})}
 `;
 
 export const ProductPrice = styled.span`
   font-size: 20px;
   font-weight: bold;
+  ${mobile({ marginBotton: "20px"})}
 `;
 
 export const Hr = styled.hr`
@@ -129,7 +131,43 @@ export const Hr = styled.hr`
 
 export const Summary = styled.div`
   flex: 1;
-  font-size: 30px;
-  font-weight: 600;
+  border: 0.5px solid #333;
+  border-radius: 5px;
+  padding: 20px;
+  height: 50vh;
   color: #000;
+`;
+
+export const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+export const SummaryItem = styled.div`
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${props => props.typeof === "total" && "500"};
+  font-size: ${props => props.typeof === "total" && "24px"};
+`;
+
+export const SummaryItemText = styled.span``;
+
+export const SummaryItemPrice = styled.span``;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #e2e2e2;
+  color: #000;
+  font-size: 1rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.6s ease;
+
+  &:hover {
+    background-color: #000;
+    color: #e2e2e2;
+  }
 `;
