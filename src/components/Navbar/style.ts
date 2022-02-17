cc35f0bf96ad630f65f14e9f3df6dbb37b2de1b7
Danilo Oliveira <oliveira.83555@gmail.com>
2022-02-16 22:32:09 -0300

    responsividade no navbar

--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -1,9 +1,12 @@
+import { findByLabelText } from "@testing-library/react";
 import styled from "styled-components";
+import { mobile } from "../../styles/Responsive";
 
 export const Container = styled.div`
   height: 60px;
   background-color: #000;
-  color: #fff
+  color: #fff;
+  ${mobile({ height: "50px", display: "flex", alignItems: "center", justifyContent: "center", position: "relative" })}
 `;
 
 export const Wrapper = styled.div`
@@ -11,6 +14,7 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
 export const Left = styled.div`
@@ -19,6 +23,7 @@ export const Left = styled.div`
   justify-content: space-between;
   align-items: center;
   border: 1px solid #e2e2e2;
+  ${mobile({ display: "none" })}
 
   input {
   width: 470px;
@@ -32,10 +37,14 @@ export const Left = styled.div`
 export const Center = styled.div`
   flex: 1;
   text-align: center;
-
   h1 {
     font-weight: bold;
+    ${mobile({ fontSize: "24px" })}
   }
+`;
+
+export const Cart = styled.div`
+  ${mobile({ position: "absolute", left: "1rem", fontSize: "1rem", cursor: "pointer" })}
 `;
 
 export const Right = styled.div`
@@ -43,12 +52,14 @@ export const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
+  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
 export const MenuItem = styled.div`
   font-size: 0.8rem;
   font-weight: bold;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 
 
